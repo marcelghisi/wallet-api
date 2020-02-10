9131a5f66287c4c031c5c1172db2038d33299de5
Marcel Ghisi <marcel.ghisi@outlook.com>
2020-02-10 23:20:32 +0000

    Config test unit

---
+++ src/test/java/com/chest/wallet/HelloWord.java
@@ -0,0 +1,16 @@
+package com.chest.wallet;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class HelloWord {
+
+  @Test
+  public void test1(){
+    Assertions.assertEquals(true,true);
+  }
+
+}
